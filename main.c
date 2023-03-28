d380ab31cfd9c5d751734235f01578a23e8d5fc9
Elkhan <74042126+ubbeg2000@users.noreply.github.com>
2023-03-29 04:25:12 +0700

    Update main.c

--- main.c
+++ main.c
@@ -6,53 +6,93 @@
  *   Deskripsi           :
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define MAX_QUEUE_SIZE 100 //asumsikan jumlah playlist tidak lebih dari 100
 
 typedef struct {
     int front;
     int rear;
-    char data[MAX_QUEUE_SIZE][100];
+    char data[MAX_QUEUE_SIZE][50];
 } Queue;
 
-void initQueue(...) {
-    ...
+void initQueue(Queue *q) {
+    q->front = q->rear = 0;
 }
 
-int isQueueEmpty(...) {
-    ...
+int isQueueEmpty(Queue *q) {
+    return (q->front == q->rear);
 }
 
-void enqueue(...) {
-    ...
+void enqueue(Queue *q, char *value) {
+    strcpy(q->data[q->rear], value);
+    q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
 }
 
-char *dequeue(...) {
-    ...
+char *dequeue(Queue *q) {
+    char *value;
+    value = q->data[q->front];
+    q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+    return value;
 }
 
-void printQueue(...) {
-    ...
+void printQueue(Queue *q) {
+    printf("Daftar lagu dalam playlist:\n");
+    if (isQueueEmpty(q)) {
+        printf("Playlist kosong\n");
+    } else {
+        int i = q->front;
+        int count = 1;
+        while (i != q->rear) {
+            printf("%d. %s\n", count, q->data[i]);
+            i = (i + 1) % MAX_QUEUE_SIZE;
+            count++;
+        }
+    }
 }
 
 int main() {
-    ...
-    char judul[100];
+    Queue q;
+    initQueue(&q);
+
+    int choice = 0;
+    char title[100];
 
     printf("Menu:\n");
     printf("1. Tambahkan lagu ke dalam playlist\n");
     printf("2. Putar lagu selanjutnya\n");
     printf("3. Tampilkan daftar lagu dalam playlist\n");
 
-    printf("\nPilihan Anda: ");
-    scanf("%d", ...);
+    do {
+        printf("\nPilihan Anda: ");
+        scanf("%d", &choice);
 
-    printf("Masukkan judul lagu: ");
-    scanf(" %[^\n]", judul); //Gunakan ini untuk input string dengan spasi
-    printf("Lagu %s berhasil ditambahkan ke dalam playlist\n", ...);
+        switch (choice) {
+            case 1:
+                printf("Masukkan judul lagu: ");
+                scanf(" %[^\n]", title); //Gunakan ini untuk input string dengan spasi
+                enqueue(&q, title);
+                printf("Lagu %s berhasil ditambahkan ke dalam playlist\n", title);
+                break;
+            case 2:
+                if (isQueueEmpty(&q)) {
+                    printf("Tidak ada lagu yang dapat diputar\n");
+                } else {
+                    char *nextSong = dequeue(&q);
+                    printf("Lagu %s sedang diputar\n", nextSong);
+                }
+                break;
+            case 3:
+                printQueue(&q);
+                break;
+            default:
+                printf("Pilihan tidak valid!\n");
+                break;
+        }
 
-    printf("Tidak ada lagu yang dapat diputar\n");
-    printf("Lagu %s sedang diputar\n", ...);
-    printf("Pilihan tidak valid!\n");
+    } while (choice != 3);
 
     return 0;
 }
